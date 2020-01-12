3fc771301cbba6dacff99615042e2ac8177b7458
Daniel Carpenter <d_c176@txstate.edu>
2020-01-12 02:10:24 -0600

    adds line for spacing

--- src/Showings.js
+++ src/Showings.js
@@ -3,6 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import { Link as RouterLink } from 'react-router-dom'
 
 const useStyles = theme => ({
   root: {
@@ -31,10 +32,14 @@ class AutoGrid extends React.Component {
             <Paper className={classes.button}>Movie</Paper>
           </Grid>
           <Grid item xs>
-            <Button className={classes.button} color="default" variant="contained">time1</Button>
+			<Button className={classes.button} 
+            component={RouterLink} to="/listings/showing"
+			color="default" variant="contained">time1</Button>
           </Grid>
           <Grid item xs>
-		  	<Button className={classes.button} color="default" variant="contained">time2</Button>
+			  <Button className={classes.button}
+			  component={RouterLink} to="/listings/showing"
+			  color="default" variant="contained">time2</Button>
           </Grid>
         </Grid>
         <Grid container>
@@ -42,10 +47,14 @@ class AutoGrid extends React.Component {
             <Paper className={classes.button}>Movie</Paper>
           </Grid>
           <Grid item xs>
-		  	<Button className={classes.button} color="default" variant="contained" spacing={0}>time1</Button>
+			  <Button className={classes.button}
+			  component={RouterLink} to="/listings/showing"
+			  color="default" variant="contained" spacing={0}>time1</Button>
           </Grid>
           <Grid item xs>
-			  <Button className={classes.button} color="default" variant="contained">time2</Button>
+			  <Button className={classes.button}
+			  component={RouterLink} to="/listings/showing"
+			  color="default" variant="contained">time2</Button>
           </Grid>
         </Grid>
       </div>
