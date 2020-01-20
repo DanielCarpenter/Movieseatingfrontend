a983965bfaece6344402ebdf45a9dd20bab5e8a3
Daniel Carpenter <d_c176@txstate.edu>
2020-01-20 14:11:31 -0600

    proper comments

--- src/App.js
+++ src/App.js
@@ -9,20 +9,22 @@ import EnhancedTable from './Showings';
 function App() {
   return (
     <BrowserRouter>
-    
+    {/* Base URL Component Loads */}
      <Route exact path='/' render={(props) => <NavBar {...props} message={"Movie Seating Powered by React and Material UI"} /> } />
      <Route exact path='/' component={SignIn} />
      
+     {/* Register Component Loads */}
      <Route exact path='/register' render={(props) => <NavBar {...props} message={"Movie Seating Powered by React and Material UI"} /> } />
      <Route exact path='/register' component={SignUp} />
      
+     {/* Protected Movie Listing URL Component Loads */}
      <Route exact path='/listings' render={(props) => <NavBar {...props} message={"Select Movie Time"} /> } />
      <ProtectedRoute exact path='/listings' component={EnhancedTable} />
+
+     {/* Protected Movie Seating URL Component Loads */}
      <Route exact path='/listings/showing' render={(props) => <NavBar {...props} message={"Select Seats"} /> } />
      <ProtectedRoute exact path='/listings/showing' component={CheckboxContainer} />
-     
-
-     
+         
     </BrowserRouter>
   );
 }
